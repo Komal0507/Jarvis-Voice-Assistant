6f825ac999e837f3fc3053a1597676d352bbef1a
Komal0507 <komalsharma11937@gmail.com>
2025-10-04 11:10:39 +0530

    Add files via upload
    Initial Commit

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "perfect": "https://www.youtube.com/watch?v=kPhpHvnnn0Q",
+    "blue": "https://www.youtube.com/watch?v=GFGPSx6cPN0",
+    "baby": "https://www.youtube.com/watch?v=kffacxfA7G4",
+    "believer": "https://www.youtube.com/watch?v=W0DM5lcj6mw",
+    "weapon": "https://www.youtube.com/watch?v=5VubhvUQaD8",
+    "Hanuman":"https://www.youtube.com/watch?v=AETFvQonfV8"
+
+}
